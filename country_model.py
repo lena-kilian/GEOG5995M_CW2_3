963723db9c19122c3692def7e0577331e65674c3
Lena Kilian <gylk@leeds.ac.uk>
2018-11-14 16:29:37 +0000

    Updated models

---
+++ country_model.py
@@ -0,0 +1,7 @@
+'''
+If I do it by country I can divide the colours into regions?
+
+Then I will also add some correlations.
+
+Could also find a way to select countries and then make chart for individual countries? --> maybe too much
+'''
--- region_model.py
+++ region_model.py
@@ -31,4 +31,6 @@ animation = animation.FuncAnimation(fig, update_graph, frames = len(cba_rgn), re
 If I do it by country I can divide the colours into regions?
 
 Then I will also add some correlations.
+
+Could also find a way to select countries and then make chart for individual countries? --> maybe too much
 '''
